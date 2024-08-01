88d8ae6181fc8e67a3a6f63ce2c6ccdc41ab4d16
Gabriel Correia <gabriel.correia@suporterenova.onmicrosoft.com>
2024-08-01 14:10:08 -0300

    feat: create get unit services

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { GetUnitsService } from 'src/app/services/get-units.service';
 
 @Component({
   selector: 'app-forms',
@@ -10,13 +11,19 @@ export class FormsComponent implements OnInit {
   results = [];
   formGroup!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private unitService: GetUnitsService
+  ) {}
 
   ngOnInit(): void {
-    this.formGroup = this.formBuilder.group({
-      hour: '',
-      showClosed: false,
-    });
+    this.unitService.getAllUnits().subscribe((data) => console.log(data));
+    {
+      this.formGroup = this.formBuilder.group({
+        hour: '',
+        showClosed: false,
+      });
+    }
   }
 
   onSubmit(): void {
